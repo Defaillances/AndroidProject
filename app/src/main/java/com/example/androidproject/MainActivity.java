343811e9771b2802c22b966d1f1fb04842d1b91f
Defaillance <mathieu.son@live.fr>
2019-02-15 08:20:25 +0100

    Initial Commit

--- app/src/main/java/com/example/androidproject/MainActivity.java
+++ app/src/main/java/com/example/androidproject/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
     }
     @Override
