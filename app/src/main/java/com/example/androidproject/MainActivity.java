3f800b949eb4bfd13046ca41e0d5d9f6628f4f69
Defaillance <mathieu.son@live.fr>
2019-02-15 09:19:43 +0100

    Initial Commit

--- app/src/main/java/com/example/androidproject/MainActivity.java
+++ app/src/main/java/com/example/androidproject/MainActivity.java
@@ -4,10 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
-    public static void main(String[] args) {
-        System.out.println("Hello, World");
-    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
